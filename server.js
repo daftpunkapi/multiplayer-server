3412c043163eb8041a2ef57246faebf99f4f23a9
Raghav Mehta <49563157+raghavm23@users.noreply.github.com>
2023-02-24 11:27:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
